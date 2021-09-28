282fcd4a9712d325df569ab4e297615ff4f8256c
azharos <azhartkj1@gmail.com>
2021-09-28 15:28:30 +0700

    kategori

---
+++ resources/views/home/kategori_all.blade.php
@@ -0,0 +1,68 @@
+@extends('layout.frontend')
+
+@section('content')
+    <section class="w-100 border-top pt-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 mb-3">
+                    <div class="btn btn-success mb-2">PascaBayar</div>
+                    <div class="row">
+                        <div class="col-lg-3 mb-2">
+                            <img src="https://via.placeholder.com/500x200" alt="" class="img-fluid">
+                        </div>
+                        <div class="col-lg-3 mb-2">
+                            <img src="https://via.placeholder.com/500x200" alt="" class="img-fluid">
+                        </div>
+                        <div class="col-lg-3 mb-2">
+                            <img src="https://via.placeholder.com/500x200" alt="" class="img-fluid">
+                        </div>
+                        <div class="col-lg-3 mb-2">
+                            <img src="https://via.placeholder.com/500x200" alt="" class="img-fluid">
+                        </div>
+                        <div class="col-lg-3 mb-2">
+                            <img src="https://via.placeholder.com/500x200" alt="" class="img-fluid">
+                        </div>
+                        <div class="col-lg-3 mb-2">
+                            <img src="https://via.placeholder.com/500x200" alt="" class="img-fluid">
+                        </div>
+                        <div class="col-lg-3 mb-2">
+                            <img src="https://via.placeholder.com/500x200" alt="" class="img-fluid">
+                        </div>
+                        <div class="col-lg-3 mb-2">
+                            <img src="https://via.placeholder.com/500x200" alt="" class="img-fluid">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 mb-3">
+                    <div class="btn btn-success mb-2">Bayar E-Commerce</div>
+                    <div class="row">
+                        <div class="col-lg-3 mb-2">
+                            <img src="https://via.placeholder.com/500x200" alt="" class="img-fluid">
+                        </div>
+                        <div class="col-lg-3 mb-2">
+                            <img src="https://via.placeholder.com/500x200" alt="" class="img-fluid">
+                        </div>
+                        <div class="col-lg-3 mb-2">
+                            <img src="https://via.placeholder.com/500x200" alt="" class="img-fluid">
+                        </div>
+                        <div class="col-lg-3 mb-2">
+                            <img src="https://via.placeholder.com/500x200" alt="" class="img-fluid">
+                        </div>
+                        <div class="col-lg-3 mb-2">
+                            <img src="https://via.placeholder.com/500x200" alt="" class="img-fluid">
+                        </div>
+                        <div class="col-lg-3 mb-2">
+                            <img src="https://via.placeholder.com/500x200" alt="" class="img-fluid">
+                        </div>
+                        <div class="col-lg-3 mb-2">
+                            <img src="https://via.placeholder.com/500x200" alt="" class="img-fluid">
+                        </div>
+                        <div class="col-lg-3 mb-2">
+                            <img src="https://via.placeholder.com/500x200" alt="" class="img-fluid">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [HomeController::class, 'index'])->name('myshop');
+Route::get('/shop/kategori', [HomeController::class, 'kategori_all'])->name('kategori.all');
